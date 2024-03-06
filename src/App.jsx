0199ac254b074bdea8143ea49c9388dbbf20bbb2
simon01092024 <simonkudus510@gmail.com>
2024-03-06 08:32:39 -0800

    Lab 3

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,11 @@ const skills = [
 ];
 
 export default function App() {
-  // const [skills, setSkills] = useState([
-  // " ",
-  // " "
-  // ]);
+  //const showSkills =  useState(skills);
+  const [skills, setSkills] = useState([{ name: "HTML", level: 5 },
+  { name: "CSS", level: 3 },
+  { name: "JavaScript", level: 4 },
+  { name: "Python", level: 2 },]);
 
   return (
     <div className='App'>
