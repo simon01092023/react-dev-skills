1d907a178a1a9bb2a504fbc457c82831ba1a6c84
simon01092024 <simonkudus510@gmail.com>
2024-03-06 20:03:52 -0800

    Lab 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SkillList from './SkillList/SkillList'
-import NewSkillForm from './NewSkillForm/NewSkillForm'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import SkillList from './SkillList/SkillList';
+import NewSkillForm from './NewSkillForm/NewSkillForm';
 
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
 
 export default function App() {
   //const showSkills =  useState(skills);
@@ -19,17 +13,25 @@ export default function App() {
   { name: "JavaScript", level: 4 },
   { name: "Python", level: 2 },]);
 
+  //const [] = useState(); boilder plate
+  const [showSkills, setShowSkills] = useState(true);
+  //skill perimeter respresent the form data argument 
+  function addSkill(skill) {
+    //replace state
+    setSkills([...skills, skill]);
+   
+  }
+
   return (
     <div className='App'>
       {/* <h1> */}
       <h1>React Dev Skills</h1>
-      {/* <SkillList> */}
-      <SkillList skills={skills} />
-
+      {/* <SkillList>  constionally render/set by useState*/}
+      {showSkills && <SkillList skills={skills} />}
       {/* <hr> */}
       <hr />
       {/* <NewSkillForm> */}
-      <NewSkillForm />
+      <NewSkillForm addSkill={addSkill} />
     </div>
 
   );
--- src/NewSkillForm/NewSkillForm.jsx
+++ src/NewSkillForm/NewSkillForm.jsx
@@ -1,20 +1,57 @@
 import "./NewSkillForm.css";
-export default function NewSkillForm() {
+import { useState } from "react";
+
+export default function NewSkillForm(props) {
+    const [formData, setFormData] = useState({
+        name: "",
+        level: "3",
+    });
+
+    console.log(props);
+    function handleAddSkill(e) {
+        e.preventDefault();
+        props.addSkill(formData);
+    }
+
+
+    function handleChange(e) {
+        const newFormData = { ...formData, [e.target.name]: e.target.value };
+        setFormData(newFormData)
+    }
+
+
     //App logic return UI as JSX
     return (
-        <form className={"NewSkillForm"}>
-            <label for="skillName">Skill</label>
-            <input type="text" id="skillName" className="skillName" />
-            <label for="skillLevel">Level</label>
-            <select id="skillLevel" name="skillLevel">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <button type="submit">ADD SKILL</button>
-        </form>
+        <>
+            <form className="NewSkillForm" onSubmit={handleAddSkill}>
+                <label>Skill
+                    <input
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        id="skillName"
+                        className="skillName"
+                        required // required the below pattern prop
+                        pattern=".{1,}"
+                    />
+                </label>
+                <label>Level
+                    <select
+                        id="skillLevel"
+                        name="level"
+                        value={formData.level}
+                        onChange={handleChange}
+                        className="skillLevel">
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                    </select>
+                </label>
+                <button type="submit">ADD SKILL</button>
+            </form>
+        </>
     );
 }
 
